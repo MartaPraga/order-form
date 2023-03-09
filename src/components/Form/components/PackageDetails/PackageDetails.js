12596461cde0aeecf5efeb502affdbcc8ca904dc
MartaPraga <mkkruk@gmail.com>
2023-03-09 16:04:08 +0100

    added popup massages after clicking parcelLocker company

--- src/components/Form/components/PackageDetails/PackageDetails.js
+++ src/components/Form/components/PackageDetails/PackageDetails.js
@@ -8,30 +8,37 @@ export const PackageDetails = () => {
   const [isDeliveryCompanyList, setDeliveryCompanyList] = useState(false);
   const [isDatePicker, setDatePicker] = useState(false);
   const [currentPackage, setCurrentPackage] = useState(0);
+  const [inPostMassage, setInPostMassage] = useState(false);
+  const [DPDpickupMassage, setDPDpickupMassage] = useState(false);
+  const [DHLPaczkomtMassage, setDHLPaczkomatMassage] = useState(false);
 
   const PACKAGE_OPTIONS = [
     {
       icon: <img src="img/small_box_icon.svg" alt="small box icon" />,
       size: 'mała',
       range: 'do 5kg',
+      lockerRange: '64 x 38 x 8 cm',
       id: 1,
     },
     {
       icon: <img src="img/medium_box_icon.svg" alt="medium box icon" />,
       size: 'średnia',
       range: 'do 10kg',
+      lockerRange: '64 x 38 x 19 cm',
       id: 2,
     },
     {
       icon: <img src="img/big_box_icon.svg" alt="big box icon" />,
       size: 'duza',
       range: 'do 20kg',
+      lockerRange: '64 x 38 x 41 cm',
       id: 3,
     },
     {
       icon: <img src="img/huge_box_icon.svg" alt="huge box icon" />,
       size: 'wielka',
       range: 'do 35kg',
+      lockerRange: '-',
       id: 4,
     },
     {
@@ -60,6 +67,24 @@ export const PackageDetails = () => {
     setCurrentPackage(id);
   };
 
+  const handleinPost = () => {
+    setInPostMassage(true);
+    setDPDpickupMassage(false);
+    setDHLPaczkomatMassage(false);
+  };
+
+  const handleDPDpickup = () => {
+    setDPDpickupMassage(true);
+    setInPostMassage(false);
+    setDHLPaczkomatMassage(false);
+  };
+
+  const handleDHLPaczkomat = () => {
+    setDPDpickupMassage(false);
+    setInPostMassage(false);
+    setDHLPaczkomatMassage(true);
+  };
+
   return (
     <Section
       title={
@@ -77,9 +102,7 @@ export const PackageDetails = () => {
           className={
             isDeliveryCompanyList
               ? 'clickedDeliveryOption'
-              : 'PackageDetails__option' && isParcelLockerList
-              ? 'block'
-              : 'clickedDeliveryOption'
+              : 'PackageDetails__option'
           }
           onClick={handleDeliveryCompanyList}
         >
@@ -90,9 +113,7 @@ export const PackageDetails = () => {
           className={
             isParcelLockerList
               ? 'clickedDeliveryOption'
-              : 'PackageDetails__option' && isDeliveryCompanyList
-              ? 'block'
-              : 'clickedDeliveryOption'
+              : 'PackageDetails__option'
           }
           onClick={handleParcelLockerList}
         >
@@ -126,23 +147,59 @@ export const PackageDetails = () => {
         {isParcelLockerList && (
           <div className="CompanyList">
             <div className="CompanyList__options">
-              <div className="CompanyList__options__option">
+              <div
+                className="CompanyList__options__option"
+                onClick={handleinPost}
+              >
                 <img src="img/logo_InPost-paczkomat.png" alt="logo Inpost" />
               </div>
-              <div className="CompanyList__options__option">
+              <div
+                className="CompanyList__options__option"
+                onClick={handleDPDpickup}
+              >
                 <img src="img/logo_DPD pickup.png" alt="logo DPD" />
               </div>
-              <div className="CompanyList__options__option">
+              <div
+                className="CompanyList__options__option"
+                onClick={handleDHLPaczkomat}
+              >
                 <img src="img/logo_DHL_paczkomat.png" alt="logo DHL" />
               </div>
             </div>
           </div>
         )}
       </div>
+      {inPostMassage ? (
+        <div className="massage">
+          Po potwierdzeniu zgłoszenia, kod do paczkomatu zostanie wysłany na
+          wskazany adres e-mail. Aby nadać przesyłkę, udaj się do paczkomatu i
+          wprowadź na ekranie otrzymany kod. Otworzy się skrytka, umieść w niej
+          paczkę i zamknij drzwiczki.
+        </div>
+      ) : (
+        <div></div>
+      )}
+      {DHLPaczkomtMassage ? (
+        <div className="">
+          Wydrukuj etykietę i zanieś paczkę do wybranego punktu obsługi paczek
+          lub DHL BOX.
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      {DPDpickupMassage ? (
+        <div>
+          Wydrukuj etykietę i zanieś paczkę do wybranego punktu obsługi paczek
+        </div>
+      ) : (
+        <div></div>
+      )}
 
       <p>Kategoria przesyłki:</p>
 
       <div className="packageSize">
+
         {PACKAGE_OPTIONS.map(({ id, icon, size, range }) => {
           const active = currentPackage === id;
 
@@ -154,18 +211,23 @@ export const PackageDetails = () => {
               >
                 <div className="packageSize__option--image">{icon}</div>
                 <div className="packageSize__option--text">
-                  <label for="id" className="packageSize__option--title">{size}</label>
-                  <div>{range}</div>                  
+                  <label for="id" className="packageSize__option--title">
+                    {size}
+                  </label>
+                  <div>{range}</div>
                 </div>
-                
               </div>
-              <input type="radio" id={id} value="id" name={size} checked={active === true}  />
-
+              <input
+                type="radio"
+                id={id}
+                value="id"
+                name={size}
+                checked={active === true}
+              />
             </div>
           );
         })}
       </div>
-
     </Section>
   );
 };
